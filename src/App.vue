5fbd94fc956e36dab66586accb31970cc254eb2f
Christopher Miles <mail@christophermiles.co.uk>
2018-06-03 18:52:51 +0100

    Added example of GET request and displaying results

--- src/App.vue
+++ src/App.vue
@@ -12,28 +12,50 @@
                     <input type="text" class="form-control" v-model="user.email">
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                <hr>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="(u,index) in users" :key="index">
+                        {{u.username}} - {{ u.email}}
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    const API_URL = 'https://vue-http-9a8c3.firebaseio.com/data.json';
+    
     export default {
         data() {
             return {
                 user: {
                     username: '',
                     email: ''
-                }
+                },
+                users: []
             }
         },
         methods: {
             submit() {
-                this.$http.post('https://vue-http-9a8c3.firebaseio.com/data.json', this.user)
+                this.$http.post(API_URL, this.user)
                     .then(response => {
                         console.log(response);
                     }, error => {
                         console.log(error);
+                    });
+            },
+            fetchData() {
+                this.$http.get(API_URL)
+                    .then(response => {
+                        return response.json();
+                    }, error => {
+                        console.log(error);
+                    })
+                    .then(data => {
+                        this.users = data;
                     });
             }
         }
